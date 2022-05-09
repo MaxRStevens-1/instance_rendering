fcaf4f9eae6421611eb3e030ebe9be50b8125a26
EndreSzakal <szakalet@dukes.jmu.edu>
2022-05-09 17:47:32 -0400

    normal

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ let num;
 
 let degrees = 1;
 
+let offsets;
+
 function render() {
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(1, 0.5, 0, 1);
@@ -26,11 +28,93 @@ function render() {
   shaderProgram.unbind();
 }
 
+// function render() {
+//   gl.viewport(0, 0, canvas.width, canvas.height);
+//   gl.clearColor(1, 0.5, 0, 1);
+//   gl.clear(gl.COLOR_BUFFER_BIT);
+
+//   shaderProgram.bind();
+//   vao.bind();
+//   degrees = degrees * Math.PI / 180;
+//   shaderProgram.setUniform1f('radians', degrees);
+
+//   for (let i = 0; i < num; i++) {
+//     shaderProgram.setUniform2f('offset', offsets[i/2], offsets[i/2+1])
+//     vao.drawIndexed(gl.TRIANGLES);  
+//   }
+//   vao.unbind();
+//   shaderProgram.unbind();
+// }
+
 function onResizeWindow() {
   canvas.width = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
   render();
 }
+
+// async function initialize() {
+//   canvas = document.getElementById('canvas');
+//   window.gl = canvas.getContext('webgl2');
+
+//   const positions = [
+//     0, 0, 0,
+//     0.01, 0, 0,
+//     0.01, 0.01, 0,
+//   ];
+
+//   const indices = [
+//     0, 1, 2,
+//   ]
+
+//   offsets = []
+//   num = 150000
+//   for (let i = 0; i < num; i++) {
+//     const ran_x = Math.random()*2 - 1
+//     const ran_y = Math.random()*2 - 1
+//     offsets.push(ran_x, ran_y)
+//   }
+
+//   attributes = new VertexAttributes();
+//   attributes.addAttribute('position', positions.length / 3, 3, positions);
+//   attributes.addIndices(indices)
+
+//   const vertexSource = `
+// uniform float radians;
+// in vec3 position;
+// uniform vec2 offset;
+
+// out vec3 mixColor;
+
+// void main() {
+//   gl_PointSize = 10.0;
+//   gl_Position = vec4(
+//     position.x * cos(radians) - position.y * sin(radians) + offset.x,
+//     position.x * sin(radians) + position.y * cos(radians) + offset.y,
+//     position.z, 1.0);
+    
+//     mixColor = vec3(position.x + offset.x, 
+//     position.y + offset.y, sin(offset.x));
+// }
+//   `;
+
+//   const fragmentSource = `
+// out vec4 fragmentColor;
+
+// in vec3 mixColor;
+
+// void main() {
+//   fragmentColor = vec4(mixColor, 1.0);
+// }
+//   `;
+
+//   shaderProgram = new ShaderProgram(vertexSource, fragmentSource);
+//   vao = new VertexArray(shaderProgram, attributes);
+
+//   // Event listeners
+//   window.addEventListener('resize', onResizeWindow);
+//   onResizeWindow();
+//   animateFrame()
+// }
 
 async function initialize() {
   canvas = document.getElementById('canvas');
